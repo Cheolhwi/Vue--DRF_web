d54449fd504ff8c977df8d5ff841786860041a6b
Cheolhwi <73050978+Cheolhwi@users.noreply.github.com>
2023-01-27 22:19:27 +0800

    add testing for order

--- djackets_django/order/tests.py
+++ djackets_django/order/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from order.models import Order
+from order.models import OrderItem
+from django.contrib.auth.models import User
+from product.models import Product
 
-# Create your tests here.
+class OrderTestCase(TestCase):
+    def setUp(self):
+        user1 = User.objects.create(username='test', password='test')
+        Order.objects.create(user=user1, first_name='test', last_name='test')
+
+    def test_order_create(self):
+        order = Order.objects.get(first_name='test')
+        self.assertEqual(order.first_name, 'test')
+        self.assertEqual(order.last_name, 'test')
+
+Í
